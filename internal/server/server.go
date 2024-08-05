4b72692c2ad07d4f3c9d685a1484b70244fbf345
AnatolKzh <bolshoymedvedtoliano@gmail.com>
2024-08-05 10:58:55 +0200

    added cors

--- internal/server/server.go
+++ internal/server/server.go
@@ -7,6 +7,7 @@ import (
 	"github.com/antlko/goauth-boilerplate/internal/server/handlers"
 	"github.com/antlko/goauth-boilerplate/internal/server/middlewares"
 	"github.com/gofiber/fiber/v3"
+	"github.com/gofiber/fiber/v3/middleware/cors"
 	"github.com/jmoiron/sqlx"
 )
 
@@ -17,6 +18,10 @@ type Config struct {
 
 func InitServer(cfg Config, dbInst *sqlx.DB) error {
 	app := fiber.New()
+	app.Use(cors.New(cors.Config{
+		AllowHeaders: []string{"Origin", "Content-Type", "Accept", "Content-Length", "Access-Control-Allow-Origin", "Access-Control-Allow-Headers", "Accept-Language", "Content-Length", "Authorization"},
+		AllowOrigins: []string{"*", "Access-Control-Allow-Headers"},
+	}))
 
 	userRepo := db.NewUserRepo(dbInst)
 	authorizer := jwt.NewAuthorizer(cfg.JwtConfig)
